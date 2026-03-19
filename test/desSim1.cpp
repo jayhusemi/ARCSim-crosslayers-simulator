438b6e3539263214ab5af1994d1039b6f91c0a76
Jay Hu <jayhusemi@gmail.com>
2026-03-19 09:21:11 -0700

    Enhance event and memory management in desSim1
    Refactor event handling and memory operations in the simulation.

--- test/desSim1.cpp
+++ test/desSim1.cpp
@@ -17,10 +17,26 @@ class scheduler(){
 			return 0; //if no err
 		}
 		int pop_event (){
-			event = queue.pop(); 
+			event ev = queue.pop(); 
+			ev.fptr_cb();
 			//if no err, add error catch
 			return 0;
 		}
+	   //overwrite event comparator for priority queue
+	   operator>(event & x, event &y){
+	   if (x.time+x.delay) > (y.time+y.delay) x >y;
+	   else x<y;
+	   }
+
+	   //run loop
+	   int run(){
+		   unint_64 g_time=0;
+		   unint_64 sim_time;
+		   while(g_time++ < sim_time ){
+		      if (queue.empty ==0) pop_event();
+			   
+		   }
+	   }
 
 	private:
 		priority_queue queue; 
@@ -30,12 +46,27 @@ class scheduler(){
 class cpu (){
 	scheduler & sch;
 	event ev;
+	mem & myMem;
 	public:
+	    int cpu( scheduler& sch, mem & mem ){
+			sch = sch;
+			myMem = mem;
+		}
 	    int mem_wr(){
+			ev.time = NOW;
+			ev.delay=10;
+			ev.alpha=0;
+			ev.src="cpu_mem_wr";
+			ev.fptr_cb= myMem.mem_wr();
 			sch.add_event(ev);
         	return 0; 
 	    }
 	    int mem_rd(){
+			ev.time = NOW;
+			ev.delay=10;
+			ev.alpha=0;
+			ev.src="cpu_mem_wr";
+			ev.fptr_cb= myMem.mem_rd();
 			sch.add_event(ev);
         	return 0; 
 	    }
@@ -48,16 +79,30 @@ class cpu (){
 
 class mem(){
 	public:
-	int mem_wr(){
+	int mem_wr(int addr, int wr_data){
 	    mem_data[addr]= wr_data;	
 	    return 0;//if no err
 		}
-	int mem_rd(){
+	int mem_rd(int addr){
 	    rd_data = mem_data[addr];	
-	    return 0;//if no err
+	    return rd_data;//if no err
 		}
 
 	private:
 	 array<int> mem_data; //dynamic array
 
+}
+
+void main(){
+	mem myMem;
+	
+	scheduler mySch;
+     cpu myCPU(mySch);
+
+	
+	myCPU.mem_wr(sch);
+	myCPU.mem_rd();
+	mySch.run();
+	
+	
 }
